e820094b3a115c2907af19e2cf25d12f15b8c99a
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2021-12-12 10:34:18 +0800

    Fixed typos in Greek.  Changed navbar highlight colour

--- src/app/modules/routing/app-routing.module.ts
+++ src/app/modules/routing/app-routing.module.ts
@@ -1,23 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
-import { HomeComponent } from '../user/components/home/home.component';
 import { TeamComponent } from '../cms/digenes/components/team/team.component';
 import { BooksComponent } from '../cms/digenes/components/books/books.component';
-import { ErrorComponent } from '../cms/digenes/components/error/error.component';
 import { RootComponent } from '../cms/digenes/components/root/root.component';
-import { AuthComponent } from '../cms/digenes/components/auth/auth.component';
 import { RedirectComponent } from '../cms/digenes/components/redirect/redirect.component';
 
 const routes: Routes = [
   { path: '', component: RootComponent },
-  { path: 'auth', component: AuthComponent },
   { path: 'team', component: TeamComponent },
   { path: 'books', component: BooksComponent },
-  { path: 'viewer', loadChildren: () => import('../viewer/viewer.module').then(m => m.ViewerModule) },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'preorder', component: RedirectComponent},
-  { path: '**', component: ErrorComponent }
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
