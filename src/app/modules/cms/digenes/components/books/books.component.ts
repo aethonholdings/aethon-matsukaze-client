033ea8470e6abd16451660d2b84413dd8f43e9e6
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2021-12-12 11:09:54 +0800

    added action for purchase on Books page

--- src/app/modules/cms/digenes/components/books/books.component.ts
+++ src/app/modules/cms/digenes/components/books/books.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { Observable } from 'rxjs';
-import { PublicationService } from 'src/app/modules/publication/services/publication.service';
 
 @Component({
   selector: 'matsukaze-books',
@@ -12,15 +9,9 @@ export class BooksComponent implements OnInit {
 
   books: any[][] = [["1", "2"], ["3", "4"], ["5", "6"]];
 
-  constructor(
-    private translateService: TranslateService,
-    private publicationService: PublicationService
-  ) { }
+  constructor() { }
 
   ngOnInit(): void {
-    // this.publicationService.get$([1]).subscribe(response => {
-    //   console.log(response);
-    // });
   }
 
 }
