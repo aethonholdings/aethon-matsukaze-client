28b99e4ebcb2dedd0653f7bf993bccfc6d9c9c13
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2022-05-15 16:17:42 +0800

    Order component made more versatile with text and link inputs

--- src/app/modules/cms/digenes/components/order/order.component.ts
+++ src/app/modules/cms/digenes/components/order/order.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'matsukaze-order',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order.component.scss']
 })
 export class OrderComponent implements OnInit {
+
+  @Input() text: string;
+  @Input() link: string;
 
   constructor() { }
 
--- src/app/modules/cms/digenes/components/redirect/redirect.component.ts
+++ src/app/modules/cms/digenes/components/redirect/redirect.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'matsukaze-redirect',
@@ -7,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RedirectComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
-    window.location.href = "https://jemmacomics.com/digenhs-i-to-simadi-tou-kain";
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    let path: string = "";
+    this.route.queryParams
+      .subscribe(params => {
+        path = params.path;
+        if(path) window.location.href = "https://jemmacomics.com/" + path;
+        else window.location.href = "https://jemmacomics.com/index.php?route=product/search&search=%CE%B4%CE%B9%CE%B3%CE%B5%CE%BD%CE%AE%CF%82";
+      }
+    );
   }
-
 }
